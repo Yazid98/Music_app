3acce15da8280f2a57c2c8ba9f5e755a7f499aa0
Yazid98 <ericbadarou@gmail.com>
2021-07-07 10:13:13 +0200

    First Commit

---
+++ lib/musique.dart
@@ -0,0 +1,13 @@
+class Musique{
+  String titre="";
+  String artiste="";
+  String imagePath="";
+  String urlSong="";
+
+  Musique(String titre, String artiste, String imagePath, String urlSong){
+    this.artiste = artiste;
+    this.titre = titre;
+    this.imagePath = imagePath;
+    this.urlSong = urlSong;
+  }
+}
